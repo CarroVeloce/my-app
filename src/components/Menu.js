44897d81a8ce67cad12db8561d510731389bc905
CarroVeloce <132054434+CarroVeloce@users.noreply.github.com>
2023-07-03 19:50:58 +0700

    Update Menu.js
    tambahin perkedel di

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -116,6 +116,11 @@
         SEMUR JENGKOL
       </label>
       <br />
+          <label style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+        <input type="checkbox" name="checkbox3" value="checkbox3" style={{ marginRight: '5px' }} />
+        PERKEDEL
+      </label>
+      <br />
 {/* BUAT PILIH LAUK*/}
 
             <button className="tombol remove-from-cart" onClick={() => removeFromCart({ name: 'NASI LAUK 1', price: 10000 })}>
