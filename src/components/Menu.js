5cefb5120e5ec00d327ce69ec5cb4f3ca239288e
CarroVeloce <132054434+CarroVeloce@users.noreply.github.com>
2023-07-03 19:53:18 +0700

    Update Menu.js
    hapus perkedel

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -116,11 +116,6 @@
         SEMUR JENGKOL
       </label>
       <br />
-          <label style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
-        <input type="checkbox" name="checkbox3" value="checkbox3" style={{ marginRight: '5px' }} />
-        PERKEDEL
-      </label>
-      <br />
 {/* BUAT PILIH LAUK*/}
 
             <button className="tombol remove-from-cart" onClick={() => removeFromCart({ name: 'NASI LAUK 1', price: 10000 })}>
