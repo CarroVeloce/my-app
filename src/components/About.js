ebedfcdfe50abb3c9e7a09c2d48acec0bcab9740
CarroVeloce <132054434+CarroVeloce@users.noreply.github.com>
2023-07-07 07:16:47 +0700

    mengubah deskripsi dan mnegubah foto di about

--- src/components/About.js
+++ src/components/About.js
@@ -15,8 +15,8 @@ const About = () => {
             <div
               class="relative mx-4 -mt-4 w-full overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]"
               data-te-ripple-init data-te-ripple-color="light">
-              <img src="https://cdn.discordapp.com/attachments/1125426619230732308/1126144544296140880/Snapinsta.app_308000733_1133744150892544_3270023260099902961_n_1080.jpg" class="w-full" />
-              <a href="#!">
+              <img src="https://cdn.discordapp.com/attachments/1125426619230732308/1126663862666657943/160777445_529837201357748_3318328582058695100_.jpg" class="w-full" />
+              <a href="https://www.facebook.com/nomouth.gun">
                 <div
                   class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
                 </div>
@@ -29,7 +29,7 @@ const About = () => {
               Web Developer
             </h6>
             <p class="text-white">
-            Saya biasa dipanggil Razita. Saya adalah seorang mahasiswa di Politeknik Negeri Jakarta. Saya berumur 19 tahun yang hobi mendengarkan musik dan membaca. Kulineran juga termasuk kesukaan saya.
+            Saya biasa dipanggil Razita. Saya adalah seorang mahasiswa di Politeknik Negeri Jakarta. Saya berumur 19 tahun yang hobi mendengarkan musik dan bermain game Minecraft.
             </p>
           </div>
         </div>
@@ -42,7 +42,7 @@ const About = () => {
             <div
               class="relative mx-4 -mt-4 w-full overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]"
               data-te-ripple-init data-te-ripple-color="light">
-              <img src="https://cdn.discordapp.com/attachments/1125426619230732308/1126147542468546581/211087835_589596702048464_2471471540522376876_n.jpg" class="w-half" />
+              <img src="https://cdn.discordapp.com/attachments/1125426619230732308/1126663862922530836/226415811_606376957037105_5029654793496987403_.jpg" class="w-half" />
               <a href="https://instagram.com/razitanazlafeb?igshid=MzNlNGNkZWQ4Mg">
                 <div
                   class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
